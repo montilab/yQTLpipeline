ee603289c0578d900791aa0811af1ce59059a22b
Mengze Li (Vanessa) <vmli@bu.edu>
2025-05-07 16:04:19 -0400

    output assoc_SNPinfo when no QTL
    even if there's no QTL from the previous step, the assoc_SNPinfo rds will still be published. in the past, the publish will fail since the phenotype name is grabbed in the QTL result file.

--- bin/4_G_merge_info_QTL.R
+++ bin/4_G_merge_info_QTL.R
@@ -11,17 +11,22 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
-sink(paste0("4_", gsub(".*assoc_(.+).rds.*", "\\1", args[2]), "_merge_SNP_info.log"), append = FALSE, split = TRUE)
+rds_file_name <- gsub(".*assoc_(.+).rds.*", "\\1", args[2])
+sink(paste0("4_", rds_file_name, "_merge_SNP_info.log"), append = FALSE, split = TRUE)
 date()
 
 gds_name <- args[1]
 QTL_res <- readRDS(args[2])
 SNP_info <- read.csv(args[3])
-phenoname <- QTL_res$phenotype[1]
-
-merged <- merge(x = SNP_info, y = QTL_res, by = "variant.id", all = F)
-
-saveRDS(merged, paste0("assoc_SNPinfo_", phenoname, "_", gds_name, ".rds"))
+if (nrow(QTL_res) > 0) {
+  phenoname <- QTL_res$phenotype[1]
+  merged <- merge(x = SNP_info, y = QTL_res, by = "variant.id", all = F)
+  saveRDS(merged, paste0("assoc_SNPinfo_", phenoname, "_", gds_name, ".rds"))
+} else {
+  warning("Warning: ", sub("^noQTL_", "", rds_file_name), " has no QTL result. \n")
+  merged <- merge(x = SNP_info, y = QTL_res, by = "variant.id", all = F)
+  saveRDS(merged, paste0("assoc_SNPinfo_", rds_file_name, ".rds"))
+}
 
 date()
 sink()
